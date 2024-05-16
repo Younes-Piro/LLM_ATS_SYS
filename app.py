d1dbbca3b34b0120c19c6e2868077f8e6ba639b6
Younes-Piro <piroyounes148@gmail.com>
2024-05-16 15:47:34 +0100

    Json issue fixed

--- app.py
+++ app.py
@@ -1,25 +1,41 @@
 from components.resume_parser import resume_parser
-from utils.helpers import read_pdf_path, format_resume, clean_json_string
+from utils.helpers import read_pdf_path, format_resume, extract_json
 import os
 from dotenv import load_dotenv
 from components.openai_parser import parse_resume
+import json
+from components.gemini_generate import generate_nonsafety_settings
+
 
 load_dotenv()
 
 os.environ["LANGCHAIN_TRACING_V2"]="true"
 os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
 
-cv_path = './data/cv.pdf'
+cv_path = './data/cv_2.pdf'
 
 cv_context = read_pdf_path(cv_path)
 chain = parse_resume(cv_context)
 result = chain.invoke({"input": cv_context})
 
-# Specify the file path
-file_path = "output_piro.txt"
+template = f"I'm working on a project that involves parsing JSON responses. Sometimes, the JSON produced by my code contains errors or issues. I'd like assistance in fixing any issues that may arise. \
+    Below is an example of JSON data that I've encountered:\
+    INPUT: {result} \
+    Could you please review this JSON and solve any errors or issues that you find?\
+    IMPORT: RETURN ONLY THE JSON"
 
-# Open the file in write mode ('w')
-with open(file_path, 'w') as file:
-    file.write(str(result))
+response = generate_nonsafety_settings(template)
 
-print("Result has been saved to", file_path)
+
+# Extract JSON from the string
+json_string = extract_json(response.strip())
+
+# Parse the extracted JSON
+try:
+    json_data = json.loads(json_string)
+    data = format_resume(json_data[0])
+except json.JSONDecodeError as e:
+    print("Error decoding JSON:", e)
+
+
+print(data)
--- utils/helpers.py
+++ utils/helpers.py
@@ -22,6 +22,31 @@ def read_pdf_path(cv_path):
     txt = retData.getvalue()
     return txt
 
+def fix_json(json_str):
+    try:
+        json.loads(json_str)
+        return json_str
+    except json.JSONDecodeError as e:
+        error_position = e.pos
+        # Check if the error position is within the string length
+
+        if error_position < len(json_str):
+            # Remove the character at the error position
+            json_str = json_str[: (error_position-2)] + json_str[error_position:]
+            return json_str
+        else:
+            return json_str
+
+def extract_json(input_string):
+    # Find the start and end indices of the JSON string
+    start_index = input_string.find("```json") + len("```json")
+    end_index = input_string.find("\n```", start_index)
+
+    # Extract the JSON string
+    json_string = input_string[start_index:end_index]
+
+    return json_string
+
 # convert experiences duration to specific format
 def convert_to_standard_format(date_str):
     # Try to parse the date using dateutil.parser
@@ -40,12 +65,12 @@ def extract_experience_duration(experiences):
 
     # Iterate through each experience in the provided list
     for exp in experiences:
-        if not any(keyword in exp["position"].lower() for keyword in ["internship", "intern", "stage", "pfe", "stagiaire"]):
+        if not any(keyword in exp["Poste"].lower() for keyword in ["internship", "intern", "stage", "pfe", "stagiaire"]):
             # Use the convert_to_standard_format function for start_date
-            start_date_str = convert_to_standard_format(exp['duration']['start_date'])
+            start_date_str = convert_to_standard_format(exp['date_de_debut'])
 
             # Use the end_date from the experience if it contains any string, otherwise use datetime.now()
-            end_date_str = convert_to_standard_format(exp['duration']['end_date']) if exp['duration']['end_date'] else None
+            end_date_str = convert_to_standard_format(exp['date_de_fin']) if exp['date_de_fin'] else None
             end_date_str = end_date_str or datetime.now().strftime("%Y-%m")
             
             # Create a tuple representing the unique date pair
@@ -76,34 +101,37 @@ def months_difference(start_date_str, end_date_str):
 def all_responsibilies(experiences):
     all_resp = []
     for exp in experiences:
-        if not any(keyword in exp["position"].lower() for keyword in ["internship", "intern", "stage", "pfe", "stagiaire"]):
-            if isinstance(exp['responsibilities'], list):
-                all_resp.extend(exp['responsibilities'])
+        if not any(keyword in exp["Poste"].lower() for keyword in ["internship", "intern", "stage", "pfe", "stagiaire"]):
+            if isinstance(exp['responsabilites'], list):
+                all_resp.extend(exp['responsabilites'])
             else:
-                all_resp.extend([exp['responsibilities']])
+                all_resp.extend([exp['responsabilites']])
     return all_resp   
 
 def format_resume(cv_parsed):
   data = {}
 
   #basic information
-  for basic in cv_parsed['basic_information'].keys():
-    data[f'{basic}'] = cv_parsed['basic_information'][f'{basic}']
+
+  basic_infos = set(cv_parsed.keys()) - set('Experiences Langages_de_programmation_technologies'.split())
+
+  for basic in basic_infos:
+    data[f'{basic}'] = cv_parsed[f'{basic}']
 
   # numero mois d'experience
-  data['months_experiences'] = extract_experience_duration(cv_parsed['professional_experiences'])
+  data['months_experiences'] = extract_experience_duration(cv_parsed['Experiences'])
 
   # responsibilites
-  data['responsibilities'] = all_responsibilies(cv_parsed['professional_experiences'])
+  data['responsibilities'] = all_responsibilies(cv_parsed['Experiences'])
 
   #Skills 
   # Check the type of the value under 'skills'
-  if isinstance(cv_parsed['skills'], list):
+  if isinstance(cv_parsed['Langages_de_programmation_technologies'], list):
     # If it's a list, return the values directly under 'skills'
-    values = cv_parsed['skills']
+    values = cv_parsed['Langages_de_programmation_technologies']
   else:
     # If it's a dictionary, assume one key and return its values
-    values = list(cv_parsed['skills'].values())[0]
+    values = list(cv_parsed['Langages_de_programmation_technologies'].values())[0]
 
   data['Programming_languages'] = values
 
